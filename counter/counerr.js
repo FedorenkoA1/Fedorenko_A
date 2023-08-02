b9ad755f647ca5cfc386d74ff9a84933a17b4658
FedorenkoA1 <and7812413df@gmail.com>
2023-08-02 21:55:16 +0300

    counters

---
+++ counter/counerr.js
@@ -0,0 +1,17 @@
+let counter = 0
+function counterMinus() {
+    if (counter > -10) {
+        counter--
+    }
+    console.log(counter)
+}
+
+function counterPlus() {
+    if (counter < 10) {
+        counter++
+    }
+    console.log(counter)
+}
+
+
+
